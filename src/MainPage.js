a30d1a9d6dc24cb1e8613ea3855b9d0454809e29
Gina Corsiglia <gina@ginasaysyes.com>
2018-09-21 16:05:12 -0700

    Coverted to stateless component

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,49 +2,45 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Shelf from './Shelf';
 
-class MainPage extends Component {
+function MainPage(props) {
 	
-	render() {
+	return (
 		
-		const { books, moveShelf } = this.props;
-
-		return(
-			<div className="list-books">
-        <div className="list-books-title">
-        	<h1>MyReads</h1>
-      	</div>
+		<div className="list-books">
+      <div className="list-books-title">
+       	<h1>MyReads</h1>
+      </div>
             
-      	<div className="list-books-content">
-        	<div>
-        		<Shelf
-        			currentShelf="currentlyReading"
-        			books={books}
-        			moveShelf={moveShelf}
-        		/>
+      <div className="list-books-content">
+        <div>
+        	<Shelf
+        		currentShelf="currentlyReading"
+        		books={props.books}
+        		moveShelf={props.moveShelf}
+        	/>
         		
-        		<Shelf 
-        			currentShelf="wantToRead"
-							books={books}
-        			moveShelf={moveShelf}
-        		/>
+        	<Shelf 
+        		currentShelf="wantToRead"
+						books={props.books}
+        		moveShelf={props.moveShelf}
+        	/>
         		
-        		<Shelf 
-        			currentShelf="read"
-							books={books}
-        			moveShelf={moveShelf}
-        		/>
-        	</div>
-      	</div>
+        	<Shelf 
+        		currentShelf="read"
+						books={props.books}
+        		moveShelf={props.moveShelf}
+        	/>
+        </div>
+      </div>
             
-      	<div className="open-search">
-        	<Link 
-        		to="/Search">
-        			Add a book
-        	</Link>
-      	</div>
-    	</div>
-		);
-	}
+      <div className="open-search">
+        <Link 
+        	to="/Search">
+        		Add a book
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default MainPage;
